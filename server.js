b1be292bc68fcbc52d5d9b0b744c0dd22c2f76ca
riccardo <dellasantar@gmail.com>
2020-05-21 23:32:54 +0200

    windows linux env var

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const ANIMAL = process.env.NAME;
-  
+const ANIMAL = "cicciobello"; //process.env.FAV_ANIMAL;
+
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
+  while (true) {
     console.log(`I really like ${ANIMAL}`);
     await sleep(5000);
   }
